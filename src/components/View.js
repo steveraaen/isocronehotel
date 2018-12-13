d31cb826b6ac603de03d1196dbf38d144fb621ac
Steveraaen <sraaen@gmail.com>
2018-12-13 18:20:31 -0500

    keys gitignored

--- src/components/View.js
+++ src/components/View.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import ReactMapboxGl, { GeoJSONLayer } from "react-mapbox-gl";
 import axios from 'axios'
+import pk from '../env.js'
 
 const mapboxStyle = "mapbox://styles/mapbox/streets-v9"
 var w = window.innerWidth * .8;
 var h = window.innerHeight;
 const Map = ReactMapboxGl({
-  					accessToken: "pk.eyJ1Ijoic3JhYWVuIiwiYSI6ImNqcGxudTVwaDBnNGQ0OW1sZ2NhcjVyZm8ifQ.J6sCIBi_knv8qU39mHqprA"
+  					accessToken: pk
 					
 			});
 export default class View extends Component {
@@ -20,7 +21,7 @@ constructor(props) {
 
 componentDidMount() {
 
-	var url = 'https://api.mapbox.com/isochrone/v1/mapbox/walking/-3.703790,40.416775?contours_minutes=5,10,15&contours_colors=6706ce,04e813,4286f4&polygons=true&access_token=pk.eyJ1Ijoic3JhYWVuIiwiYSI6ImNqcGxudTVwaDBnNGQ0OW1sZ2NhcjVyZm8ifQ.J6sCIBi_knv8qU39mHqprA'
+	var url = 'https://api.mapbox.com/isochrone/v1/mapbox/walking/-3.703790,40.416775?contours_minutes=5,10,15&contours_colors=6706ce,04e813,4286f4&polygons=true&access_token=' + pk
 		axios.get(url)
 		  .then( (response) =>{
 		  	this.setState({
